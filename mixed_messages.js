ede0bbd50d55d1ab593ea87d373963cf95b42e9a
catraf <93711577+catraf@users.noreply.github.com>
2021-11-10 19:14:54 +1100

    Create parts of the generator

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,11 +1,21 @@
 // A catchphrase generator that will generate a random catchphrase.
 
-const catchphrase_parts = {
+// starting bank of catchphrase parts
+const catchphraseParts = {
+  start_word: ['howzat', 'look at that', 'you', 'wow', 'gabba', 'jazza'],
   silly_word: ['jazzle', 'balloo', 'jingle', 'bingle', 'balong',
       'malarkey'],
-  start_word: ['howzat', 'look at that', 'you'],
-  end_word: ['boo', 'it', 'ha'],
+  end_word: ['boo', 'it', 'ha', 'zang'],
 };
 
+const catchprhaseArray = [];
+
+Object.values(catchphraseParts).forEach (val => {
+  num = Math.floor(Math.random() * val.length);
+  catchprhaseArray.push(val[num].toUpperCase());
+})
+
+let generatedCatchphrase = `${catchprhaseArray.join(" ")}!!!`;
+console.log(generatedCatchphrase);
 
 
