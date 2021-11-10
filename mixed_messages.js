e1215c853c7fe1b3db18146921dbcfbd523b336a
catraf <93711577+catraf@users.noreply.github.com>
2021-11-10 18:37:16 +1100

    Add starting bank of catchphrase parts

---
+++ mixed_messages.js
@@ -0,0 +1,11 @@
+// A catchphrase generator that will generate a random catchphrase.
+
+const catchphrase_parts = {
+  silly_word: ['jazzle', 'balloo', 'jingle', 'bingle', 'balong',
+      'malarkey'],
+  start_word: ['howzat', 'look at that', 'you'],
+  end_word: ['boo', 'it', 'ha'],
+};
+
+
+
